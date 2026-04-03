b6d88a70be2ded35a5f460c920a7cd2b0e93b81c
MelekKalyoncuTufanç <mkalyonc@hotmail.com>
2026-04-03 10:58:18 +0300

    initialize spring boot project structure

---
+++ src/main/java/com/example/docprocessing/DocumentProcessingApplication.java
@@ -0,0 +1,12 @@
+package com.example.docprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentProcessingApplication.class, args);
+    }
+}
